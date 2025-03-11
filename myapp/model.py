6b5687cfb2af59b32223c8035e0b359f5f575712
redstarht <185753202+redstarht@users.noreply.github.com>
2025-03-11 21:22:50 +0900

    20250311

--- myapp/model.py
+++ myapp/model.py
@@ -14,6 +14,7 @@ class Accounts(UserMixin, db.Model):
     email = db.Column(db.String(100), unique=True, default='', nullable=False)
     display_name = db.Column(db.String(100), default='', nullable=False)
     role = db.Column(db.Integer, default=1, nullable=False)
+    # 廃盤flag （0: 現役, 1: 廃止）
     is_deleted = db.Column(db.Boolean, default=False, nullable=False)
     created_at = db.Column(db.DateTime, default=lambda: datetime.now(
         timezone(timedelta(hours=9))))
@@ -96,9 +97,9 @@ class Subsection(db.Model):
     # 廃盤flag （0: 現役, 1: 廃止）
     is_deleted = db.Column(db.Boolean, default=False, nullable=False)
     # 作成ユーザーID（NULL許容）
-    created_by = db.Column(db.Integer, nullable=True)
+    created_by = db.Column(db.Integer,db.ForeignKey('accounts.id'), nullable=True)
     # 更新ユーザーID（NULL許容）
-    updated_by = db.Column(db.Integer, nullable=True)
+    updated_by = db.Column(db.Integer,db.ForeignKey('accounts.id'), nullable=True)
     created_at = db.Column(db.DateTime, default=lambda: datetime.now(
         timezone(timedelta(hours=9))))
     updated_at = db.Column(
@@ -106,6 +107,9 @@ class Subsection(db.Model):
             timezone(timedelta(hours=9))),
         onupdate=lambda: datetime.now(timezone(timedelta(hours=9)))
     )
+
+    creator = db.relationship('Accounts',foreign_keys=[created_by],lazy='joined',backref='created_subsection')
+    updater = db.relationship('Accounts',foreign_keys=[updated_by],lazy='joined',backref='updated_subsection')
     production_lines = db.relationship('Production_line',backref='subsection',lazy=True)
     employees = db.relationship('Employee',backref='subsection',lazy=True)
 
@@ -119,7 +123,9 @@ class Subsection(db.Model):
             'sort_order': self.sort_order,
             'is_deleted': self.is_deleted,
             'created_by': self.created_by,
+            'created_by_username':self.creator.username if self.creator else None,
             'updated_by': self.created_by,
+            'updated_by_username':self.updater.username if self.updater else None,
             'created_at': self.created_at.strftime('%Y-%m-%d %H:%M:%S') if self.created_at else None,
             'updated_at': self.updated_at.strftime('%Y-%m-%d %H:%M:%S') if self.updated_at else None,
             'production_lines': [prod_line.to_dict() for prod_line in self.production_lines],
@@ -136,8 +142,9 @@ class Production_line(db.Model):
     code = db.Column(db.String(50), default='', nullable=False)
     sort_order = db.Column(db.Integer, default=500, nullable=True)
     is_deleted = db.Column(db.Boolean, default=False, nullable=False)
-    created_by = db.Column(db.Integer, nullable=True)
-    updated_by = db.Column(db.Integer, nullable=True)
+    created_by = db.Column(db.Integer,db.ForeignKey('accounts.id'), nullable=True)
+    # 更新ユーザーID（NULL許容）
+    updated_by = db.Column(db.Integer,db.ForeignKey('accounts.id'), nullable=True)
     created_at = db.Column(db.DateTime, default=lambda: datetime.now(
         timezone(timedelta(hours=9))))
     updated_at = db.Column(
@@ -145,6 +152,9 @@ class Production_line(db.Model):
             timezone(timedelta(hours=9))),
         onupdate=lambda: datetime.now(timezone(timedelta(hours=9)))
     )
+    creator = db.relationship('Accounts',foreign_keys=[created_by],lazy='joined',backref='created_production_lines')
+    updater = db.relationship('Accounts',foreign_keys=[updated_by],lazy='joined',backref='updated_production_lines')
+    # abnormal_infos = db.relationship('Abnormal_info', backref='production_line', lazy=True)
 
     def to_dict(self):
         return {
@@ -155,9 +165,12 @@ class Production_line(db.Model):
             'sort_order': self.sort_order,
             'is_deleted': self.is_deleted,
             'created_by': self.created_by,
+            'created_by_username':self.creator.username if self.creator else None,
             'updated_by': self.created_by,
+            'updated_by_username':self.updater.username if self.updater else None,
             'created_at': self.created_at.strftime('%Y-%m-%d %H:%M:%S') if self.created_at else None,
-            'updated_at': self.updated_at.strftime('%Y-%m-%d %H:%M:%S') if self.updated_at else None
+            'updated_at': self.updated_at.strftime('%Y-%m-%d %H:%M:%S') if self.updated_at else None,
+            # 'abnormal_infos': [abnormal_info.to_dict() for abnormal_info in self.abnormal_infos]
         }
 
 
@@ -168,10 +181,11 @@ class Employee(db.Model):
         'subsections.id'), nullable=False)
     name = db.Column(db.String(100), nullable=False)
     code = db.Column(db.String(50), default='', nullable=False)
+    position_id = db.Column(db.Integer,nullable=False,default=1)
     sort_order = db.Column(db.Integer, default=500, nullable=True)
     is_deleted = db.Column(db.Boolean, default=False, nullable=False)
-    created_by = db.Column(db.Integer, nullable=True)
-    updated_by = db.Column(db.Integer, nullable=True)
+    created_by = db.Column(db.Integer,db.ForeignKey('accounts.id'), nullable=True)
+    updated_by = db.Column(db.Integer,db.ForeignKey('accounts.id'), nullable=True)
     created_at = db.Column(db.DateTime, default=lambda: datetime.now(
         timezone(timedelta(hours=9))))
     updated_at = db.Column(
@@ -179,6 +193,8 @@ class Employee(db.Model):
             timezone(timedelta(hours=9))),
         onupdate=lambda: datetime.now(timezone(timedelta(hours=9)))
     )
+    creator = db.relationship('Accounts',foreign_keys=[created_by],lazy='joined',backref='created_employee')
+    updater = db.relationship('Accounts',foreign_keys=[updated_by],lazy='joined',backref='updated_employee')
 
     def to_dict(self):
         return {
@@ -186,10 +202,52 @@ class Employee(db.Model):
             'name': self.name,
             'subsection_id': self.subsection_id,
             'code': self.code,
+            'position':self.position,
             'sort_order': self.sort_order,
             'is_deleted': self.is_deleted,
             'created_by': self.created_by,
+            'created_by_username':self.creator.username if self.creator else None,            
             'updated_by': self.created_by,
+            'updated_by_username':self.updater.username if self.updater else None,            
             'created_at': self.created_at.strftime('%Y-%m-%d %H:%M:%S') if self.created_at else None,
             'updated_at': self.updated_at.strftime('%Y-%m-%d %H:%M:%S') if self.updated_at else None
         }
+    
+# class Abnormal_info(db.Model):
+#     __tablename__ = 'abnormal_infos'
+#     id = db.Column(db.Integer, primary_key=True)
+#     subsection_id = db.Column(db.Integer, db.ForeignKey(
+#         'subsections.id'), nullable=False)
+#     name = db.Column(db.String(100), nullable=False)
+#     code = db.Column(db.String(50), default='', nullable=False)
+#     position = db.Column(db.Integer,nullable=False,default=1)
+#     sort_order = db.Column(db.Integer, default=500, nullable=True)
+#     is_deleted = db.Column(db.Boolean, default=False, nullable=False)
+#     created_by = db.Column(db.Integer,db.ForeignKey('accounts.id'), nullable=True)
+#     updated_by = db.Column(db.Integer,db.ForeignKey('accounts.id'), nullable=True)
+#     created_at = db.Column(db.DateTime, default=lambda: datetime.now(
+#         timezone(timedelta(hours=9))))
+#     updated_at = db.Column(
+#         db.DateTime, default=lambda: datetime.now(
+#             timezone(timedelta(hours=9))),
+#         onupdate=lambda: datetime.now(timezone(timedelta(hours=9)))
+#     )
+#     creator = db.relationship('Accounts',foreign_keys=[created_by],lazy='joined',backref='created_abnormal_info')
+#     updater = db.relationship('Accounts',foreign_keys=[updated_by],lazy='joined',backref='updated_abnormal_info')
+
+#     def to_dict(self):
+#         return {
+#             'id': self.id,
+#             'name': self.name,
+#             'subsection_id': self.subsection_id,
+#             'code': self.code,
+#             'position':self.position,
+#             'sort_order': self.sort_order,
+#             'is_deleted': self.is_deleted,
+#             'created_by': self.created_by,
+#             'created_by_username':self.creator.username if self.creator else None,            
+#             'updated_by': self.created_by,
+#             'updated_by_username':self.updater.username if self.updater else None,            
+#             'created_at': self.created_at.strftime('%Y-%m-%d %H:%M:%S') if self.created_at else None,
+#             'updated_at': self.updated_at.strftime('%Y-%m-%d %H:%M:%S') if self.updated_at else None
+#         }    
