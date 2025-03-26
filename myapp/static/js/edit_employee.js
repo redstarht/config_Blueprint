f7c18323e72d9c994109a175404019db5c32d54d
redstarht <185753202+redstarht@users.noreply.github.com>
2025-03-27 00:24:51 +0900

    250327_h1

--- myapp/static/js/edit_employee.js
+++ myapp/static/js/edit_employee.js
@@ -2,19 +2,19 @@
 
 export function setEmployeesSpreadsheet(spreadSheetId) {
     jspreadsheet(spreadSheetId, {
-        worksheets: [{
-            data: [
-                ['Hello', 13123, '', 'Yes', true, '#AA4411'],
-                ['World!', 8, '', 'No', false, '#99BE23']
-            ],
-            columns: [
-                { type: 'text', title: 'Text' },
-                { type: 'numeric', title: 'Numeric', mask: '$ #.##,00', decimal: ',' },
-                { type: 'calendar', title: 'Calendar' },
-                { type: 'dropdown', source: ['Yes', 'No', 'Maybe'] },
-                { type: 'checkbox', title: 'Checkbox' },
-                { type: 'color', title: 'Color', width: 50, render: 'square' }
-            ]
-        }]
+        data: [
+            ['Hello', 13123, '', 'Yes', true, '#AA4411'],
+            ['World!', 8, '', 'No', false, '#99BE23']
+        ],
+        columns: [
+            { type: 'hidden'}, //管理ID 非表示
+            { type: 'numeric', title: 'Numeric', mask: '$ #.##,00', decimal: ',' },
+            { type: 'calendar', title: 'Calendar' },
+            { type: 'dropdown', source: ['Yes', 'No', 'Maybe'] },
+            { type: 'checkbox', title: 'Checkbox' },
+            { type: 'color', title: 'Color', width: 50, render: 'square' }
+        ]
     })
 };
+
+{type : hidden}
